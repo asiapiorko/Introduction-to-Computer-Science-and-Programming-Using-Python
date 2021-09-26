17bb9c66bdfa2ac26cb3cabc80926724e77633b2
asiapiorko <64212078+asiapiorko@users.noreply.github.com>
2021-09-26 19:47:50 +0200

    Exercise 1 using for loops
    1. Convert the following code into code that uses a for loop.
    
    prints 2
    prints 4
    prints 6
    prints 8
    prints 10
    prints Goodbye!

---
+++ Exercise 1.py
@@ -0,0 +1,11 @@
+# Convert the following code into code that uses a for loops 
+
+for count in range(2,11,2):
+    print(count)
+print("Goodbye!")
+
+
+for count in range(10):
+    if count != 0 and count % 2 == 0:
+        print(count)
+print("Goodbye!")
